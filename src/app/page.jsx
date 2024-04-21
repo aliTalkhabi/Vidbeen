560830f780b1e43b97c349d1937138560d775395
aliTalkhabi <atdes1999@gmail.com>
2024-04-21 17:29:16 +0330

    change structure

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Header from "@/Components/Header/Header";
+import Footer from "@/Components/Footer/Footer";
+
 
 export default function Home() {
   return (
     <>
-      <header></header>
+      <Header />
       <main className={styles.main}>
         <aside></aside>
       </main>
-      <footer></footer>
+      <Footer />
+      
     </>
   );
 }
