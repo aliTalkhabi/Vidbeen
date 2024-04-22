bd0897a961fed7c68936a394ceda3c038d54c8d1
aliTalkhabi <atdes1999@gmail.com>
2024-04-22 12:21:46 +0330

    add favicon

---
+++ src/app/icon.jsx
@@ -0,0 +1,34 @@
+import { ImageResponse } from 'next/og';
+export const size = {
+    width: 20,
+    height: 20,
+}
+export const contentType = 'image/png'
+ 
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+     
+      <div
+        style={{
+          fontSize:14,
+          background: '#2a0de0',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+        }}
+      >
+        📷
+      </div>
+    ),
+    
+    {
+      
+      ...size,
+    }
+  )
+}
