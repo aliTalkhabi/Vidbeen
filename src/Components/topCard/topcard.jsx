7e14a3a8b4b216888d430ef2cc814b8e9c97f9ce
aliTalkhabi <atdes1999@gmail.com>
2024-05-07 10:10:04 +0330

    previous-2

--- src/Components/topCard/topcard.jsx
+++ src/Components/topCard/topcard.jsx
@@ -8,7 +8,7 @@ export default function TopCard() {
     return (
         <Grid container wrap="wrap" sx={{ justifyContent: 'space-evenly', alignItems: 'center' }}>
             <Typography className="topparagraph" sx={{ width: '100%', padding: '0 2rem', color: '#1B2CC1', fontWeight: '900' }}>پربازدیدترین <span className="span">ویدیو ها</span> </Typography>
-            <Box sx={{width:{xs:'350px',overflowY:'hidden'}}}>
+            <Box sx={{width:{xs:'370px',overflowY:'hidden'}}}>
                 <Box sx={{ display: "grid", width: { xs: 'max-content', md: '100%' }, gridTemplateColumns: 'repeat(3,1fr)', padding:{xs:'0 .5rem',md:'0 2rem'} , gap: '10px' }}>
                     <Cards dataCards={TopCardsInfo} />
                 </Box>
