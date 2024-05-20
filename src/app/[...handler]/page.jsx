9bc2c8c1f3001707cd499c6fbde3421266fda33f
aliTalkhabi <atdes1999@gmail.com>
2024-05-20 16:20:12 +0330

    define routes

--- src/app/[...handler]/page.jsx
+++ src/app/[...handler]/page.jsx
@@ -1,12 +1,18 @@
-'use client'
+"use client";
 import { usePathname } from "next/navigation";
 import styles from "../page.module.css";
 export default function Handler() {
-  const pathname = usePathname()
+  const pathname = usePathname();
   console.log(pathname);
   return (
     <main className={styles.main}>
-      <h1> handler</h1>
+      {pathname === "/category" ? (
+        <p>category handler</p>
+      ) : pathname === "/product" ? (
+        <p>product handler</p>
+      ) : pathname !== "/category" || pathname !== "/product" ? (
+        <p>not-found</p>
+      ) : null}
     </main>
   );
 }
