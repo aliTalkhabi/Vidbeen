19adc9f2e9a40311335e51d92355a74016e2d5e4
aliTalkhabi <atdes1999@gmail.com>
2024-07-17 11:27:56 +0430

    change to aside

--- src/Components/RightList/RightList.jsx
+++ src/Components/RightList/RightList.jsx
@@ -15,7 +15,7 @@ import StickyBox from 'react-sticky-box';
 
 export default function RightList() {
   return (
-    <Box className={styles.asideContainer}>
+    <Box component='aside' className={styles.asideContainer}>
       <StickyBox offsetTop={75} offsetBottom={0}>
         
         {RightListItems.map((rightListItem, i) => {
