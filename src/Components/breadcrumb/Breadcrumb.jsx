7830d488374d4c777bbcf0d53faa24127e88fb42
aliTalkhabi <atdes1999@gmail.com>
2024-07-20 17:50:30 +0430

    add structure for breadcrumb

--- src/Components/breadcrumb/Breadcrumb.jsx
+++ src/Components/breadcrumb/Breadcrumb.jsx
@@ -3,13 +3,43 @@ import { Link, Skeleton, Stack } from "@mui/material";
 import dynamic from "next/dynamic";
 import { usePathname } from "next/navigation";
 import React from "react";
+import Head from 'next/head';
 const BreadcrumbDynamic = dynamic(() => import("@mui/material/Breadcrumbs"), {
   ssr: false,
-  loading: () => <Skeleton variant="rectangular" animation="wave"/>,
+  loading: () => <Skeleton variant="rectangular" animation="wave" />,
 });
-export default function Breadcrumb() {
-  const pathname = usePathname();
 
+export default function Breadcrumb() {
+  const breadcrumbSchema = {
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    "itemListElement": [
+      {
+        "@type": "ListItem",
+        "position": 1,
+        "name": "خانه",
+        "item": "http://localhost:3000/"
+      },
+      {
+        "@type": "ListItem",
+        "position": 2,
+        "name": "دسته بندی",
+        "item": "http://localhost:3000/category"
+      },
+      {
+        "@type": "ListItem",
+        "position": 3,
+        "name": "زیرشاخه دسته بندی",
+        "item": "http://localhost:3000/category/subcategory"
+      }, {
+        "@type": "ListItem",
+        "position": 4,
+        "name": "محصول",
+        "item": "http://localhost:3000/category/product"
+      }
+    ]
+  }
+  const pathname = usePathname();
   const breadcrumb = [
     <Link key="1" color="inherit" href="/">
       خانه
@@ -33,16 +63,24 @@ export default function Breadcrumb() {
   ];
 
   return (
-    <Stack component="section" spacing={2} sx={{ margin: "1rem auto", width:'85%' }}>
-      <BreadcrumbDynamic component='nav' separator="»" aria-label="breadcrumb" sx={{fontFamily:'Dana-Regular'}}>
-        {pathname === "/"
-          ? breadcrumb.slice(0, 1)
-          : pathname === "/category"
-          ? breadcrumb.slice(0, 2)
-          : pathname === "/category/product"
-          ? breadcrumb.slice(0, 3)
-          : null}
-      </BreadcrumbDynamic>
-    </Stack>
+    <>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(breadcrumbSchema) }}
+        />
+      </Head>
+      <Stack component="section" spacing={2} sx={{ margin: "1rem auto", width: '85%' }}>
+        <BreadcrumbDynamic component='nav' separator="»" aria-label="breadcrumb" sx={{ fontFamily: 'Dana-Regular' }}>
+          {pathname === "/"
+            ? breadcrumb.slice(0, 1)
+            : pathname === "/category"
+              ? breadcrumb.slice(0, 2)
+              : pathname === "/category/product"
+                ? breadcrumb.slice(0, 3)
+                : null}
+        </BreadcrumbDynamic>
+      </Stack>
+    </>
   );
 }
