96ab10a760f9d262ca5680b62cc8575e332ddc94
aliTalkhabi <atdes1999@gmail.com>
2024-07-23 15:59:42 +0430

    add alt for 404 error page

--- src/app/404.jsx
+++ src/app/404.jsx
@@ -8,7 +8,7 @@ const Custom404 = () => {
     return (
         <BoxDynamic component='section' sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '1rem', overflow: 'hidden', position: 'relative', width:{xs:'95%',sm:'95%',md:'85%'}, height: '600px', margin: '5rem auto', background: 'rgb(13 166 224) 100%', boxShadow: '0 0 8px rgba(0, 0, 0, 0.6)' }}>
             <Box component='section' >
-                <Image src={UnPower} priority style={{ height: '100%', width: '100%' }} />
+                <Image src={UnPower} priority alt="بنر 404" style={{ height: '100%', width: '100%' }} />
             </Box>
             <Box component='section' sx={{ position: 'absolute', right: '5%', top: '5%', color: '#fff', fontSize: {xs:'1.5rem',sm:'1.5rem',md:'1.5rem',lg:'3rem'} }}>
                 <p>ای گمشده ، این راه </p>
