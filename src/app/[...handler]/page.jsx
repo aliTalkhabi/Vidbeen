f4c83c7f88b77d62a3f7ff602aaeaaa02558ab5f
aliTalkhabi <atdes1999@gmail.com>
2024-07-28 12:57:00 +0430

    changing menu

--- src/app/[...handler]/page.jsx
+++ src/app/[...handler]/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { usePathname } from "next/navigation";
-import styles from "../page.module.css";
+
 import Category from "@/components/Category/Category";
 import Product from "@/components/Product/Product";
 import Custom404 from "../404";
@@ -9,7 +9,7 @@ export default function Handler() {
   
   const pathname = usePathname();
   return (
-    <main className={styles.main}>
+    <>
       {pathname === "/category" ? (
         <>
           <Category />
@@ -21,6 +21,6 @@ export default function Handler() {
       ) : pathname !== "/category" || pathname !== "/product" ? (
         <Custom404 />
       ) : null}
-    </main>
+    </>
   );
 }
